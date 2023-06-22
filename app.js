cc43e717b74ce5005f6e6ea9c8eba7a7117fc648
Adrian Alvarez <alvarez0710@outlook.com>
2023-06-21 18:53:25 -0700

    changed db connection to app.js

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const inquirer = require('inquirer');
-const db = require('./database/connection');
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Adrian123&/()',
+    database: 'employeeTrackerSystemDB'
+});
 
 const init = async () => {
   try {
