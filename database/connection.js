3eecde06268c0bcd964904d1e1bbf9b95f729c6f
Adrian Alvarez <alvarez0710@outlook.com>
2023-06-21 18:52:28 -0700

    correct name db.

--- database/connection.js
+++ database/connection.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'Adrian123&/()',
-    database: 'employee_tracker_db'
+    database: 'employeeTrackerSystemDB'
 });
 
 module.exports = db;
